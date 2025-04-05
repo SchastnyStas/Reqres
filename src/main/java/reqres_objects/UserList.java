8c7cce5903106bf440e64fbf8c5c9778d0696f10
Stanislau Schastny <stasschastny@gmail.com>
2025-04-05 14:59:56 +0200

    Add Rest API

---
+++ src/main/java/reqres_objects/UserList.java
@@ -0,0 +1,15 @@
+package reqres_objects;
+
+import com.google.gson.annotations.Expose;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+@Builder
+public class UserList {
+    @Expose
+    ArrayList<UserForUsersList> data;
+
+}
